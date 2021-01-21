994d88f4e8ebdf4c272d5fb6324fad27d72a0a4b
martynas3113 <martynas.pazusis@gmail.com>
2021-01-21 23:40:10 +0200

    generated items

--- src/components/industries/industries.js
+++ src/components/industries/industries.js
@@ -1,33 +1,28 @@
 import React, { Component } from 'react'
 import { Col, Container, Row } from 'react-bootstrap';
 import './industries.scss';
+import { titles } from './industriesData'
 
 class Industries extends Component {
+    constructor(props){
+        super(props)
+        this.state =
+        {
+            data: titles,
+        }
+    }
     render() {
+        console.log(this.state.data.names)
         return (
             <Container className="ind-container">
+                
                 <Row className="ind-wrap">
                     <Col lg={6} md={6} className="ind-left">
                         <h3 className="ind-title">Industries we cover</h3>
                         <ul className="ind-list">
-                            <li>
-                             <a className="ind-item" href="#">Fintech</a>   
-                            </li>
-                            <li>
-                             <a className="ind-item" href="#">Fintech</a>   
-                            </li>
-                            <li>
-                             <a className="ind-item" href="#">Fintech</a>   
-                            </li>
-                            <li>
-                             <a className="ind-item" href="#">Fintech</a>   
-                            </li>
-                            <li>
-                             <a className="ind-item " href="#">Fintech</a>   
-                            </li>
-                            <li>
-                             <a className="ind-item" href="#">Fintech</a>   
-                            </li>
+                            {this.state.data.names.map((title ,idx) => <li key={idx}>
+                             <a className="ind-item" href="#">{title}</a>   
+                            </li>)}
                         </ul>
                     </Col>
                     <Col lg={6} md={6} className="ind-right">
---
+++ src/components/industries/industriesData.js
@@ -0,0 +1,14 @@
+export const titles ={
+    names:[
+        "Fintech",
+        "Healtcare",
+        "Media & Advertising",
+        "Retail",
+        "Education",
+        "Enterprise software",
+        "Travel & Transportation",
+        "Data mining & Processing"
+
+    ]
+}
+
