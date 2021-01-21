40a3d38269c2912cfa9693e8781747ceab6b3ac1
martynas3113 <martynas.pazusis@gmail.com>
2021-01-21 18:42:08 +0200

    added cards with responsive

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Col, Container, Row } from 'react-bootstrap';
+import { Card, CardDeck, Col, Container, Row } from 'react-bootstrap';
 import './services.scss';
 
 class Services extends Component {
@@ -7,40 +7,90 @@ class Services extends Component {
         return (
             <Container fluid className="services-container">
                 <Row className="services-wrap">
-                    <Col lg={{span:3, offset:1}} className="services-text">
+                    <Col xl={{span:3, offset:1}} lg={12} className="services-text">
                         <h2>Services we provide</h2>
                     </Col>
-                    <Col lg={8} className="services-cards">
-                       <a className="card" href="">
-                           <div className="header">
-                               <img src="./images/webdev.svg" style={{width: '50px'}} alt=""/>
-                               <h3>Title</h3>
-                           </div>
-                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, veniam.</p>
-                       </a>
-                       <a className="card" href="">
-                           <div className="header">
-                               <img src="./images/webdev.svg" style={{width: '50px'}} alt=""/>
-                               <h3>Title</h3>
-                           </div>
-                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, veniam.</p>
-                       </a>
-                       <a className="card" href="">
-                           <div className="header">
-                               <img src="./images/webdev.svg" style={{width: '50px'}} alt=""/>
-                               <h3>Title</h3>
-                           </div>
-                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, veniam.</p>
-                       </a>
-                       <a className="card" href="">
-                           <div className="header">
-                               <div className="icon">
-                               <img src="./images/webdev.svg" style={{width: '50px'}} alt=""/>
-                               </div>
-                               <h3>Title</h3>
-                           </div>
-                           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, veniam.</p>
-                       </a>
+                    <Col xl={8} lg={12} className="services-cards">
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+                            </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                            </Card.Footer>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This card has supporting text below as a natural lead-in to additional
+                                content.{' '}
+                            </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                            </Card.Footer>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This card has even longer content than the first to
+                                show that equal height action.
+                            </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                            </Card.Footer>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+                            </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                            </Card.Footer>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This card has supporting text below as a natural lead-in to additional
+                                content.{' '}
+                            </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                            </Card.Footer>
+                        </Card>
+                        <Card>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This card has even longer content than the first to
+                                show that equal height action.
+                            </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                            </Card.Footer>
+                        </Card>
                     </Col>
                 </Row>
             </Container>
