e1795544bcb4af2e30d9b8a19036caedd73cf059
martynas3113 <martynas.pazusis@gmail.com>
2021-01-26 14:11:40 +0200

    cards generated

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import './services.scss';
+import { cardsData } from './servicesData';
 
 class Services extends Component {
+    static defaultProps ={
+        data: cardsData
+    }
     render() {
         return (
             <Container fluid className="services-container">
@@ -11,66 +15,19 @@ class Services extends Component {
                         <h2>Services we provide</h2>
                     </Col>
                     <Col xl={9} lg={12} s={12} xs={12} className="services-cards">
-                        <Card className="card">
+                        {this.props.data.data.map((item, idx) => 
+                            <Card key={idx} className="card">
                             <Card.Header className="card-header">
-                            <img src="./images/webdev.svg" alt="web"/>
-                            <Card.Title className="card-title">Web development</Card.Title>
+                            <img src={item.img} alt="web"/>
+                            <Card.Title className="card-title">{item.title}</Card.Title>
                             </Card.Header>
                             <Card.Body>
                             <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
+                                {item.text}
                             </Card.Text>
                             </Card.Body>
                         </Card>
-                        <Card className="card">
-                            <Card.Header className="card-header">
-                            <img src="./images/mobile.svg" alt="web"/>
-                            <Card.Title className="card-title">Mobile development</Card.Title>
-                            </Card.Header>
-                            <Card.Body>
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
-                            </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card className="card">
-                            <Card.Header className="card-header">
-                            <img src="./images/consulting.svg" alt="web"/>
-                            <Card.Title className="card-title">It consulting</Card.Title>
-                            </Card.Header>
-                            <Card.Body>
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
-                            </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card className="card">
-                            <Card.Header className="card-header">
-                            <img src="./images/quality.svg" alt="web"/>
-                            <Card.Title className="card-title">Quality assurance</Card.Title>
-                            </Card.Header>
-                            <Card.Body className="card-body">
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
-                            </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card className="card">
-                            <Card.Header className="card-header">
-                            <img src="./images/design.svg" alt="web"/>
-                            <Card.Title className="card-title">Design solutions</Card.Title>
-                            </Card.Header>
-                            <Card.Body>
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
-                            </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        )}
                     </Col>
                 </Row>
             </Container>
---
+++ src/components/services/servicesData.js
@@ -0,0 +1,36 @@
+const cardsData = {
+    data: [
+         {
+            title: "Web development",
+            img:  "./images/webdev.svg",
+            text: "Our certified development experts use only trusted technologies for delivering back-end, front-end, cloud, AI, and other software solutions."
+        },
+        {
+            title: "Mobile Development",
+            img:  "./images/mobile.svg",
+            text: "It doesn’t matter what type of mobile software you need; our developers are ready to build native, hybrid, and cross-platform applications." 
+        },
+        {
+            title: "IT Consulting",
+            img:  "./images/consulting.svg",
+            text: "Being a team with vast experience in development allows us to provide consulting services and give our clients time- and money-saving recommendations." 
+        },
+        {
+            title: "Business Analytics",
+            img:  "./images/analytics.svg",
+            text: "The development part of product creation should go after business analysis and planning, and clients can always rely on our experts in this area." 
+        },
+        {
+            title: "Quality Assurance",
+            img:  "./images/quality.svg",
+            text: "Top quality is impossible without quality control. Our QA specialists provide automated and manual testing to make sure that created software is flawless." 
+        },
+        {
+            title: "Design Solutions",
+            img:  "./images/design.svg",
+            text: "Software products must be unique, eye-pleasing and user-friendly. Our UI/UX designers create brand identity concepts, prototypes, great interfaces, and more." 
+        }
+    ]
+}
+
+export { cardsData }
